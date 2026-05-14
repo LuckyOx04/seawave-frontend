f103b8525d4caecfd5324900637731a9fd032a1d
LuckyOx04 <ivailon27@gmail.com>
2026-05-14 23:08:32 +0300

    Added logic for shuffle for playlists and repeat for playlist or a single track.

--- SeawaveApp/Services/PlaybackManager.cs
+++ SeawaveApp/Services/PlaybackManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using LibVLCSharp.Shared;
 using SeawaveApp.Models;
@@ -10,37 +12,120 @@ public class PlaybackManager : IDisposable
 {
     private readonly LibVLC _libVlc;
     private readonly MediaPlayer _mediaPlayer;
+    
+    private List<int> _playbackOrder = [];
+    private int _orderIndex = -1;
+    private Random _rng = new();
 
-    public ObservableCollection<UnifiedTrack> TracksQueue { get; } = new();
-    public int CurrentTrackIndex { get; private set; } = -1;
+    public ObservableCollection<UnifiedTrack> TracksQueue { get; } = [];
+    
+    public RepeatMode CurrentRepeatMode { get; set; } = RepeatMode.None;
+    public bool IsShuffle { get; set; }
 
     public PlaybackManager()
     {
         _libVlc = new LibVLC();
         _mediaPlayer = new MediaPlayer(_libVlc);
-        
-        _mediaPlayer.EndReached += (s, e) => Task.Run(Next);
+        _mediaPlayer.EndReached += (s, e) => HandleTrackEnd();
     }
 
-    public void Play(UnifiedTrack track)
+    public void PlayFromPlaylist(IEnumerable<UnifiedTrack> tracks, int startIndex)
     {
-        Media? media = null;
-
-        if (track.IsRemote)
+        TracksQueue.Clear();
+        foreach (var track in tracks.Skip(startIndex))
         {
-            media = new Media(_libVlc, new Uri(track.RemoteUrl!));
+            TracksQueue.Add(track);
+        }
+
+        RebuildOrder(startIndex: 0);
+    }
+
+    public void PlaySingle(UnifiedTrack track)
+    {
+        TracksQueue.Clear();
+        TracksQueue.Add(track);
+        RebuildOrder(startIndex: 0);
+    }
+
+    public void AddToQueue(UnifiedTrack track)
+    {
+        TracksQueue.Add(track);
+        UpdateOrderForNewTrack();
+    }
+
+    private void RebuildOrder(int startIndex)
+    {
+        _playbackOrder = Enumerable.Range(0, TracksQueue.Count).ToList();
+
+        if (IsShuffle && _playbackOrder.Count > 1)
+        {
+            var current = _playbackOrder[startIndex];
+            _playbackOrder.RemoveAt(startIndex);
+            _playbackOrder = _playbackOrder.OrderBy(x => _rng.Next()).ToList();
+            _playbackOrder.Insert(0, current);
+            _orderIndex = 0;
         }
         else
         {
-            media = new Media(_libVlc, track.LocalPath!, FromType.FromPath);
+            _orderIndex = startIndex;
         }
+
+        PlayCurrent();
+    }
+
+    private void UpdateOrderForNewTrack()
+    {
+        var newIndex = TracksQueue.Count - 1;
+        if (IsShuffle)
+        {
+            var insertPosition = _rng.Next(_orderIndex + 1, _playbackOrder.Count + 1);
+            _playbackOrder.Insert(insertPosition, newIndex);
+        }
+        else
+        {
+            _playbackOrder.Add(newIndex);
+        }
+    }
+
+    private void PlayCurrent()
+    {
+        if (_orderIndex < 0 || _orderIndex >= _playbackOrder.Count)
+        {
+            return;
+        }
+        
+        var trackIndex = _playbackOrder[_orderIndex];
+        var track = TracksQueue[trackIndex];
+        var media = GetMediaForTrack(track);
+
+        _mediaPlayer.Play(media);
+    }
+
+    private Media GetMediaForTrack(UnifiedTrack track)
+    {
+        var media = track.IsRemote ? new Media(_libVlc, new Uri(track.RemoteUrl!)) 
+            : new Media(_libVlc, track.LocalPath!);
 
         if (track.StartOffset > TimeSpan.Zero)
         {
             media.AddOption($":start-time={track.StartOffset.TotalSeconds}");
         }
-        
-        _mediaPlayer.Play(media);
+
+        return media;
+    }
+
+    private void HandleTrackEnd()
+    {
+        Task.Run(() =>
+        {
+            if (CurrentRepeatMode == RepeatMode.Track)
+            {
+                PlayCurrent();
+                return;
+            }
+            
+            Next();
+        });
     }
 
     public void TogglePause()
@@ -57,22 +142,26 @@ public class PlaybackManager : IDisposable
 
     public void Next()
     {
-        if (CurrentTrackIndex >= TracksQueue.Count - 1)
+        if (_orderIndex < _playbackOrder.Count - 1)
         {
-            return;
+            _orderIndex++;
+            PlayCurrent();
         }
-        CurrentTrackIndex++;
-        Play(TracksQueue[CurrentTrackIndex]);
+        else if (CurrentRepeatMode == RepeatMode.All)
+        {
+            _orderIndex = 0;
+            PlayCurrent();
+        }
     }
 
     public void Previous()
     {
-        if (CurrentTrackIndex <= 0)
+        if (_orderIndex <= 0)
         {
             return;
         }
-        CurrentTrackIndex--;
-        Play(TracksQueue[CurrentTrackIndex]);
+        _orderIndex--;
+        PlayCurrent();
     }
 
     public void Seek(TimeSpan position) =>
@@ -82,5 +171,6 @@ public class PlaybackManager : IDisposable
     {
         _mediaPlayer.Dispose();
         _libVlc.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
