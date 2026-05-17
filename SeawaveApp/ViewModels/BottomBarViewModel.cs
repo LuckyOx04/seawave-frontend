7d93016d458aa83f78551843e53fa61f3d3c0027
LuckyOx04 <ivailon27@gmail.com>
2026-05-17 15:55:19 +0300

    Implemented BottomBarViewModel.cs.

--- SeawaveApp/ViewModels/BottomBarViewModel.cs
+++ SeawaveApp/ViewModels/BottomBarViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SeawaveApp.Models;
 using SeawaveApp.Services;
 
@@ -23,7 +24,7 @@ public partial class BottomBarViewModel : ViewModelBase
         {
             if (Math.Abs(CurrentPosition.TotalSeconds - value) > 0.9)
             {
-                //SeekToSeconds(value);
+                _playbackManager.Seek(TimeSpan.FromSeconds(value));
             }
         }
     }
@@ -33,7 +34,72 @@ public partial class BottomBarViewModel : ViewModelBase
     public BottomBarViewModel(PlaybackManager playbackManager)
     {
         _playbackManager = playbackManager;
-        
-        //CurrentTrack = _playbackManager.
+
+        CurrentTrack = _playbackManager.CurrentTrack;
+        IsPlaying = _playbackManager.IsPlaying;
+        CurrentPosition = _playbackManager.Position;
+        TrackDuration = _playbackManager.Duration;
+        IsShuffleOn = _playbackManager.IsShuffle;
+        UpdateRepeatLabel(_playbackManager.CurrentRepeatMode);
+
+        _playbackManager.TrackChanged += (_, track) => CurrentTrack = track;
+        _playbackManager.PlaybackStateChanged += (_, playing) => IsPlaying = playing;
+        _playbackManager.ShuffleChanged += (_, shuffle) => IsShuffleOn = shuffle;
+        _playbackManager.RepeatChanged += (_, mode) => UpdateRepeatLabel(mode);
+
+        _playbackManager.PositionChanged += OnPositionChanged;
+        _playbackManager.DurationChanged += OnDurationChanged;
+    }
+
+    private void OnPositionChanged(object? sender, TimeSpan position)
+    {
+        CurrentPosition = position;
+        OnPropertyChanged(nameof(CurrentPositionSeconds));
+    }
+
+    private void OnDurationChanged(object? sender, TimeSpan duration)
+    {
+        TrackDuration = duration;
+        OnPropertyChanged(nameof(TrackDurationSeconds));
+    }
+
+    private void UpdateRepeatLabel(RepeatMode mode)
+    {
+        RepeatModeLabel = mode switch
+        {
+            RepeatMode.Track => "Repeat: One",
+            RepeatMode.All => "Repeat: All",
+            _ => "Repeat: Off"
+        };
+    }
+
+    [RelayCommand]
+    private void TogglePlayPause()
+    {
+        _playbackManager.TogglePause();
+    }
+
+    [RelayCommand]
+    private void SkipNext()
+    {
+        _playbackManager.Next();
+    }
+
+    [RelayCommand]
+    private void SkipPrevious()
+    {
+        _playbackManager.Previous();
+    }
+
+    [RelayCommand]
+    private void ToggleShuffle()
+    {
+        _playbackManager.ToggleShuffle();
+    }
+
+    [RelayCommand]
+    private void CycleRepeat()
+    {
+        _playbackManager.CycleRepeat();
     }
 }
