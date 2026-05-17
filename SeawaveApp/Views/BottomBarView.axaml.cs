8762961d89a029720d8ade89a14bdc160d4e0db8
LuckyOx04 <ivailon27@gmail.com>
2026-05-17 23:59:58 +0300

    Added BottomBarView.axaml.

---
+++ SeawaveApp/Views/BottomBarView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SeawaveApp.Views;
+
+public partial class BottomBarView : UserControl
+{
+    public BottomBarView()
+    {
+        InitializeComponent();
+    }
+}
