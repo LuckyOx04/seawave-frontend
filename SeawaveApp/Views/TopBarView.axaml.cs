e070ad91926d52a9a401051d17238877fb54869c
LuckyOx04 <ivailon27@gmail.com>
2026-05-17 22:31:22 +0300

    Moved the resources from App.axaml to their own file.

---
+++ SeawaveApp/Views/TopBarView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SeawaveApp.Views;
+
+public partial class TopBarView : UserControl
+{
+    public TopBarView()
+    {
+        InitializeComponent();
+    }
+}
