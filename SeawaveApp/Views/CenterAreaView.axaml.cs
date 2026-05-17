23ca916b67bede91a3ec8e8b59d732da6391e3e7
LuckyOx04 <ivailon27@gmail.com>
2026-05-17 23:52:20 +0300

    Added CenterAreaView.axaml.

---
+++ SeawaveApp/Views/CenterAreaView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SeawaveApp.Views;
+
+public partial class CenterAreaView : UserControl
+{
+    public CenterAreaView()
+    {
+        InitializeComponent();
+    }
+}
