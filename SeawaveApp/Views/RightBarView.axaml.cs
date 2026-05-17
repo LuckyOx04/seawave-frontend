60bd5a050722776bff3cce6b9a19ee27dda36be0
LuckyOx04 <ivailon27@gmail.com>
2026-05-17 23:47:00 +0300

    Added RightBarView.axaml.

---
+++ SeawaveApp/Views/RightBarView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SeawaveApp.Views;
+
+public partial class RightBarView : UserControl
+{
+    public RightBarView()
+    {
+        InitializeComponent();
+    }
+}
