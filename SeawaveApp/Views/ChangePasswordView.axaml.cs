65ccca6e293e8c4488bd37b4b221f0ab06590a14
LuckyOx04 <ivailon27@gmail.com>
2026-05-18 23:27:12 +0300

    Added ChangePasswordView.

---
+++ SeawaveApp/Views/ChangePasswordView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SeawaveApp.Views;
+
+public partial class ChangePasswordView : UserControl
+{
+    public ChangePasswordView()
+    {
+        InitializeComponent();
+    }
+}
