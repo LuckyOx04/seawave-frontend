cc8d843ce11d8f47f8b25895b616ec9e9e7d9473
LuckyOx04 <ivailon27@gmail.com>
2026-05-30 23:49:41 +0300

    Changed RightBarViewModel.cs by addding a new CurrentDisplayTrack and changing how DisplayQueue gets initialized and updated in the UpdateDisplayTracks method.

--- SeawaveApp/ViewModels/RightBarViewModel.cs
+++ SeawaveApp/ViewModels/RightBarViewModel.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SeawaveApp.Models;
@@ -9,7 +11,8 @@ public partial class RightBarViewModel : ViewModelBase
 {
     private readonly PlaybackManager _playbackManager;
 
-    [ObservableProperty] public partial PermutationList DisplayQueue { get; private set; }
+    [ObservableProperty] public partial IList<UnifiedTrack>? DisplayQueue { get; private set; }
+    [ObservableProperty] public partial UnifiedTrack? CurrentDisplayTrack { get; private set; }
 
     public RightBarViewModel(PlaybackManager playbackManager)
     {
@@ -23,7 +26,17 @@ public partial class RightBarViewModel : ViewModelBase
 
     private void UpdateDisplayTracks()
     {
-        DisplayQueue = new PermutationList(_playbackManager.TracksQueue, _playbackManager.PlaybackOrder);
+        if (_playbackManager.PlaybackOrder.Count == 0 || _playbackManager.OrderIndex < 0)
+        {
+            CurrentDisplayTrack = null;
+            DisplayQueue = new List<UnifiedTrack>();
+            return;
+        }
+        
+        var fullPermutationList = new PermutationList(_playbackManager.TracksQueue, _playbackManager.PlaybackOrder);
+        CurrentDisplayTrack = _playbackManager.CurrentTrack;
+
+        DisplayQueue = fullPermutationList.Skip(_playbackManager.OrderIndex + 1).ToList();
     }
 
     [RelayCommand]
