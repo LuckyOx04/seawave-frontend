062ce26df8948c51ace05abd90f396f415e56439
LuckyOx04 <ivailon27@gmail.com>
2026-05-30 23:36:41 +0300

    Changed PermutationList.cs, so it now implements IReadOnlyList.

--- SeawaveApp/Models/PermutationList.cs
+++ SeawaveApp/Models/PermutationList.cs
@@ -1,8 +1,19 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace SeawaveApp.Models;
 
-public class PermutationList(List<UnifiedTrack> masterList, List<int> indexMap)
-    : ReadOnlyCollection<UnifiedTrack>(indexMap.Select(index => masterList[index]).ToList());
+public class PermutationList(List<UnifiedTrack> masterList, List<int> indexMap) : IReadOnlyList<UnifiedTrack>
+{
+    public IEnumerator<UnifiedTrack> GetEnumerator()
+    {
+        return indexMap.Select(index => masterList[index]).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public int Count => indexMap.Count;
+
+    public UnifiedTrack this[int index] => masterList[indexMap[index]];
+}
