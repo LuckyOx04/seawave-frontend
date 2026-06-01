cdfd3f4bea2f1daa813cbfef6217e47cf29bf7fa
LuckyOx04 <ivailon27@gmail.com>
2026-06-01 14:22:49 +0300

    Added the ability to play a song when doubble clicked.

--- SeawaveApp/Views/CenterAreaView.axaml.cs
+++ SeawaveApp/Views/CenterAreaView.axaml.cs
@@ -1,6 +1,10 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using SeawaveApp.Models;
+using SeawaveApp.ViewModels;
 
 namespace SeawaveApp.Views;
 
@@ -9,5 +13,30 @@ public partial class CenterAreaView : UserControl
     public CenterAreaView()
     {
         InitializeComponent();
+    }
+
+    private void Border_OnDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is not Border border)
+        {
+            return;
+        }
+
+        if (border.DataContext is not UnifiedTrack selectedTrack)
+        {
+            return;
+        }
+        
+        var userControl = border.FindAncestorOfType<UserControl>();
+
+        if (userControl?.DataContext is not CenterAreaViewModel vm)
+        {
+            return;
+        }
+
+        if (vm.PlayTrackCommand.CanExecute(selectedTrack))
+        {
+            vm.PlayTrackCommand.Execute(selectedTrack);
+        }
     }
 }
